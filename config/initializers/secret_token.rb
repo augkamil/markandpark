0633a2326a6f67a5a0406fd8e033bcce31dde84c
Kamil Augustynowicz <kamil.augustynowicz@netguru.pl>
2012-11-17 16:05:30 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Markandpark::Application.config.secret_token = '23330968e71a12f432879e11fc4da9539f1c081fd4fb2ba71964d9f8e046fd5355dceaeae52c6c8bd732fbf1a696da8562a65475f9957e24ae2a4dceaf7d0f4b'
